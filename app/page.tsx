fae1bb313c8089ac7710a6086abae641ceadbf18
Johnnyk69 <102842446+Johnnyk69@users.noreply.github.com>
2026-03-29 18:27:59 -0400

    Create Home component for AngleFinder app

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>AngleFinder</h1>
+      <p>Upload creatives. Find the winning angle.</p>
+    </div>
+  );
+}
