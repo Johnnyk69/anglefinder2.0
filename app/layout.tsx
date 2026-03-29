f72a04956170125528552a2ebcbcf6b8fc2da69a
Johnnyk69 <102842446+Johnnyk69@users.noreply.github.com>
2026-03-29 18:39:42 -0400

    Remove fontFamily style from body element

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <body style={{ fontFamily: "sans-serif" }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
